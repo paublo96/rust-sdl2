eb77adf1ed318e953b5c488451cb3d5c00ceb232
Paul Minner <minner.paul@gmail.com>
2022-02-14 22:02:29 -0800

    Remove unnecessary changes.

--- build.rs
+++ build.rs
@@ -1,14 +1,4 @@
-use std::env;
-
 fn main() {
     #[cfg(any(target_os = "openbsd", target_os = "freebsd"))]
     println!(r"cargo:rustc-link-search=/usr/local/lib");
-
-    if let Ok(sdl_path) = env::var("SDL2_STATIC_LIB_PATH") {
-        println!(
-            "cargo:rustc-link-search={}",
-            sdl_path
-        );
-    }
-
 }
